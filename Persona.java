b724f39cca5936b303122f1a1ba92adae36e2872
juan <juan191322@gmail.com>
2024-08-31 19:32:05 -0500

    Mdf Constructores Persona.java

--- Persona.java
+++ Persona.java
@@ -2,4 +2,12 @@ public class Persona {
     private String cedula;
     private String nombre;
     private String apellido;
+
+    public persona(){
+    }
+    public Persona(String cedula, String nombre, String apellido){
+        this.cedula=cedula;
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
 }
